3c54e4a8e70dafbb95041d548c62982006c3b4c8
chazorg <goni.chazor@biodata.com>
2016-02-29 16:56:07 +0200

    change account for saucelab

--- src/main/java/com/biodata/labguru/tests/BaseTest.java
+++ src/main/java/com/biodata/labguru/tests/BaseTest.java
@@ -37,7 +37,7 @@ public class BaseTest implements SauceOnDemandSessionIdProvider, SauceOnDemandAu
      * supplied by environment variables or from an external file, use the no-arg {@link SauceOnDemandAuthentication} constructor.
      */
    // public SauceOnDemandAuthentication authentication = new SauceOnDemandAuthentication("gonichazor", "bc580ecc-f8c7-4559-8337-fa6b16861e14");
-	public SauceOnDemandAuthentication authentication = new SauceOnDemandAuthentication("revitalkr", "9dbad2a7-0c75-4303-a4ae-1224d71e92fd");
+	public SauceOnDemandAuthentication authentication = new SauceOnDemandAuthentication("danainbar", "bff1466f-0523-4094-9329-69f12abc0f3c");
 
 
  
